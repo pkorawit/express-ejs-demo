c15cf9e4f3f37a68e1268994c7789d987982c1fb
Korawit <korawit.p@gmail.com>
2018-09-24 11:00:24 +0700

    Done insert update delete navigation

---
+++ students.sql
@@ -0,0 +1,272 @@
+/* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS students(
+  sid VARCHAR(100), 
+  title VARCHAR(100), 
+  firstname VARCHAR(100), 
+  lastname VARCHAR(100)
+);
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5730213073', 'MR.', 'JASADAPORN', 
+    'SAIN-UN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930211002', 'MR.', 'WORRAPONG', 
+    'ONGSAKUL'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930211010', 'MR.', 'JIRAYUS', 'PHITHAKSAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930211038', 'MR.', 'SITTISAK', 
+    'PHETKEAW'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930211040', 'MR.', 'RUSLAM', 'SAMAE'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930212014', 'MISS', 'KAMONWAN', 
+    'PLAISOPHON'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930212015', 'MISS', 'NATTHANICHA', 
+    'PHITTHAYAPHRAPHAPHORN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213003', 'MISS', 'PHATSARA', 
+    'RUANGRAT'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213005', 'MISS', 'SUTHATHIP', 
+    'MANIKAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213006', 'MISS', 'PHAPLAK', 
+    'SAETHAPAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213007', 'MR.', 'TANAWAT', 'MONPIPAT'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213009', 'MR.', 'JIRAWAT', 'PEANKEAW'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213010', 'MISS', 'NATTIDA', 
+    'NOO-BOON'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213012', 'MISS', 'NATTHAWARAPON', 
+    'TUBTIANG'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213015', 'MISS', 'TANSUTA', 
+    'RATKAEW'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213019', 'MR.', 'NOPPADON', 
+    'KAEWKONG'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213021', 'MISS', 'NISARAT', 
+    'SAELIM'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213022', 'MISS', 'WARITSARA', 
+    'BAMBUTTOOK'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213023', 'MR.', 'AEKBURUT', 
+    'RAWANGNGAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213024', 'MISS', 'RADTANAPORN', 
+    'CHOEIKAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213025', 'MISS', 'CHAWEEWAN', 
+    'SOOKWHAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213030', 'MR.', 'COFFAREE', 
+    'HAHZA'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213031', 'MR.', 'PADCHARA', 
+    'TEEDUANG'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213033', 'MISS', 'SUPAVADI', 
+    'MANASUWAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213034', 'MR.', 'TANAKORN', 
+    'PITAKCHAICHAN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213035', 'MISS', 'JIDAPA', 'TASSANAKUL'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213036', 'MR.', 'TEERAYUT', 
+    'HATSAKORN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213037', 'MR.', 'PORAMAT', 'NUKLIANG'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213038', 'MR.', 'KRITTAYOT', 
+    'ANUBOOT'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213042', 'MR.', 'JIRAKRIT', 
+    'MANU'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213043', 'MR.', 'PEERANAN', 
+    'REANMATAEN'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213044', 'MR.', 'PATHOMPHON', 
+    'SRIPANROD'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213045', 'MISS', 'CHOOTIGARN', 
+    'TANAPIBALWONGSA'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213049', 'MR.', 'WANATIP', 'TUANROPI'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213050', 'MR.', 'WACHIRAWIT', 
+    'BUMRUNGCHUA'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213054', 'MISS', 'TINAKARN', 
+    'JANTHONG'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213055', 'MR.', 'PATIPHAN', 
+    'WIBUNCHAT'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213056', 'MR.', 'PATCHARAPON', 
+    'MONDEE'
+  );
+/* INSERT QUERY */
+INSERT INTO students(sid, title, firstname, lastname) 
+VALUES 
+  (
+    '5930213057', 'MR.', 'LEKKLA', 'WILAILAK'
+  );
---
+++ subjects.sql
@@ -0,0 +1,427 @@
+/* CREATE TABLE */
+CREATE TABLE IF NOT EXISTS subjects(
+  code VARCHAR(100), 
+  title VARCHAR(100), 
+  credits VARCHAR(100), 
+  sections DECIMAL(10, 2)
+);
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-110', 'ORGANIZATION & MANAGEMENT', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-140', 'SOFTWARE&COM PROGRAMMING', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-150', 'INFORMATION TECHNOLOGY', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-211', 'PRINCIPLES OF ACCOUNTING', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-220', 'MATH FOR IT', '3 (3-0-6)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-240', 'DATA STRUCTURES', '3 (2-2-5)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-241', 'JAVA PROGRAMMING LABORATORY', 
+    '1 (0-3-0)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-250', 'DATABASE SYSTEMS', '3 (2-2-5)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-330', 'OPERATING SYS. CONCEPT&TECH', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-332', 'COMPUTER NETWORKING & MGT', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-334', 'INTERNET OF THINGS', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-340', 'WEB PROGRAMMING', '3 (2-2-5)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-380', 'ELECTRONIC BUSINESS TECH', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-390', 'WORKSHOP IN IT III(FULL STACK', 
+    '1 (0-3-0)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-393', 'PRE-COOPERATIVE EDUCATION', 
+    '1 (0-3-0)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-464', 'GAME PROGRAMMING', '3 (3-0-6)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-491', 'PROJECT IN IT II', '3 (0-9-0)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-493', 'COOPERATIVE EDUCATION', 
+    '6 (0-36-0)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-494', 'SEMINAR', '1 (0-2-1)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-495', 'ST IN IT I(PRIN OF CREA&DESIGN', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-495', 'ST IN IT I(COM GRAPH&DESIGN 2D', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '140-498', 'COMPUTING ETHICS AND LAWS', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-140', 'SOFTWARE AND COMPUTER PROG.', 
+    '3 (2-2-5)', 2
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-120', 'MATHEMATICS', '3 (3-0-6)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-231', 'COM ARCHITEC&OPERATING SYS', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-271', 'REQUIREMENT ENG&SOFTWARE', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-277', 'SERVICE-ORIENTED ARCHITEC', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-320', 'STATISTICAL METHODS', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-341', 'MOBILE DEVICE APP DEVELOP', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-360', 'HUMAN COMPUTER INTERACTION', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-375', 'SOFTWARE PROJECT MANAGEMENT', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-393', 'SEMINAR IN SE', '1 (0-2-1)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '977-491', 'PROJECT IN SOFTWARE ENGINEE', 
+    '3 (0-9-0)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-101', 'BUSINESS INFORMATION SYSTEM', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-110', 'PRINCIPLES OF E-BIZ', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-120', 'MATHEMATICS', '3 (3-0-6)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-121', 'STATISTICAL METHOD', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-130', 'COMPUTER&INTERNET TECH.', 
+    '3 (2-2-5)', 2
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-140', 'SOFTWARE AND COMPUTER PROG.', 
+    '3 (2-2-5)', 2
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-210', 'PRINCIPLES OF MARKETING', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-211', 'PRINCIPLES OF ACCOUNTING', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-220', 'MATH FOR IT', '4 (4-0-8)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-242', 'SYSTEMS ANALYSIS AND DESIGN', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-250', 'DATABASE SYSTEMS', '3 (2-2-5)', 
+    2
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-303', 'ENTERPRISE RESOURCE PLANNING', 
+    '3 (2-2-5)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-310', 'INFORMATION TECH STRATEGY', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-314', 'CONSUMER BEHAVIOR', '3 (3-0-6)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-315', 'BUSINESS RESEARCH METHODS', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-331', 'WEB DESIGN & MANAGEMENT', 
+    '3 (2-2-5)', 2
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-392', 'PRE-COOP EDUCATION', 
+    '1 (0-3-0)', 3
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-394', 'LEGAL&ETHICAL CONSIDER IN IT', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-491', 'PROJECT IN ELECTRONIC BUSINESS', 
+    '3 (0-9-0)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-493', 'SEMINAR', '1 (0-2-1)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-495', 'ST IN E-BIZ I(ORGAN DESIGN)', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-501', 'MANAGEMENT INFOR SYSTEM', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-502', 'RESEARCH METHOD IN IT', 
+    '3 (3-0-6)', 1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-503', 'STATISTICS FOR IT', '3 (3-0-6)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-505', 'SEMINAR I', '1 (0-2-1)', 
+    1
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-601', 'THESIS', '18 (0-54-0)', 
+    5
+  );
+/* INSERT QUERY */
+INSERT INTO subjects(code, title, credits, sections) 
+VALUES 
+  (
+    '976-607', 'SEMINAR III', '1 (0-2-1)', 
+    1
+  );
