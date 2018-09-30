4d444bbbbeac512a66e7c732f70d1081c06257a5
Korawit <korawit.p@gmail.com>
2018-09-30 22:54:55 +0700

    Update database URL

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var data = require('./data.js');
 const pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
+var db = pgp(process.env.DATABASE_URL);
 
 var mysql = require('mysql')
 var connection = mysql.createConnection({
